f0061f442a44075f6355d5d48699aa5a3bb125db
Jason Agus <jasonagus@Jasons-MacBook-Air.local>
2020-11-09 17:14:00 -0800

    Sets up Basic Files

---
+++ mockschema.js
@@ -0,0 +1,281 @@
+const mockSchema = 
+{
+    "Skyblock Community Guide": {
+        "Tips to Game Progression": {
+            "Early Game:": {
+
+            },		
+            
+            "Mid Game:": {
+                "General Tips:": {
+
+                },		 
+                "Moving out of the Mid Game:": {
+    
+                },	
+            },		
+            
+            "Late Game:": {
+                "General Tips:": {
+
+                },		
+                "Moving out of the Late Game:": {
+    
+                },		
+            },		
+            
+            "Late-End Game:": {
+                "General Tips:": {
+
+                },		
+                "Moving out of the Late-End Game:": {
+    
+                },	
+            },		
+
+        },
+
+        "Skills Guide": {
+            "Farming": {
+                "Items necessary to start grinding": {
+
+                },	
+                "Optimal Minion Setup": {
+    
+                },
+            },	
+	
+            "Mining": {
+                "Manual Grinding": {
+
+                },	
+                "Items Necessary:": {
+    
+                },	
+                "Optimal Minion Setup": {
+    
+                },
+            },	
+	
+            "Combat": {
+                "Early Game:": {
+
+                },	
+                "Mid Game:": {
+    
+                },	
+                "Late Game / Late-End Game:": {
+                    "Revenant (Revs)": {
+    
+                    },	
+                    "Dungeons": {
+        
+                    },
+                },	
+            },	
+            
+	
+            "Foraging [~200-600k xp/hr]": {
+
+            },	
+            
+            "Fishing": {
+                "Rods:": {
+
+                },	
+                "Armor:": {
+    
+                },	
+                "Pets:": {
+    
+                },	
+                "Bait:": {
+    
+                },
+            },	
+	
+            "Enchanting": {
+
+            },	
+            "Alchemy": {
+
+            },	
+            "Taming": {
+
+            },	
+        },
+
+        "Common Money Making Methods": {
+            "Building a Farm": {
+                "Cane (Est. 600k / hour) - 3 layers": {
+
+                },		
+                "Pumpkin (Est. ??? / hour) - 4 layers": {
+
+                },			
+                "Melons (Est. ??? / hour) - 4 layers": {
+
+                },			
+                "Netherwart (Est. ??? / hour) - ??? layers N/A": {
+
+                },			
+            },		
+
+            "Zealot Farming": {
+                "Recommended Gear to Start:": {
+
+                }
+            },		
+	
+            "Bazaar and AH Flipping": {
+
+            },	
+            
+            "Participating In Events": {
+                "Spooky Event": {
+
+                },	
+                "Fishing Festival Event": {
+
+                },		
+                "Mythological Event": {
+
+                },	
+            },		
+	
+        },
+
+        "Minion Progression": {
+            "Early Game": {
+
+            },
+            "Mid Game":	{
+
+            },
+            "Late Game": {
+
+            },	
+        },
+
+        "Recommended Pets to Buy": {
+
+        },
+
+        
+        "Slayer T3/T4 Tips": {
+
+        },
+
+        "General FAQs": {
+
+        },
+
+        "Update Tips": {
+
+        },
+    },
+
+
+    // BLOCK TEXT
+
+
+    "Dungeons Guide": {
+        "Catacombs Equipment Guide (Catacombs 0 - 25)": {
+            "F1 Armor": {
+            
+            },		
+            "F1 Weapons": {
+                
+            },		
+            "F2 Armor": {
+                
+            },		
+            "F2 Weapons": {
+                
+            },		
+            "F3 Armor": {
+                
+            },		
+            "F3 Weapons": {
+                
+            },		
+            "F4 Armor": {
+                
+            },		
+            "F4 Weapons": {
+                
+            },		
+            "F5 Armor": {
+                
+            },		
+            "F5 Weapons": {
+                
+            },		
+            "F6 Armor": {
+                
+            },		
+            "F6 Weapons": {
+                
+            },		
+        },	
+        
+        "Boss Fights": {
+            "Bonzo": {
+            
+            },		
+            "Scarf": {
+                
+            },		
+            "Professor": {
+                
+            },		
+            "Thorn": {
+                
+            },		
+            "Livid": {
+                
+            },
+        },		
+		
+        "Dungeon-Utility Pets:": {
+            "Starting Out Dungeons (Budget Pets) with Floor 1 / Entrance": {
+            
+            },		
+            "Midway through Dungeons (Floor 2 - 4)": {
+                
+            },		
+            "More than halfway through Dungeons (Floor 5+)": {
+                
+            },	
+        },		
+        	
+        "Dungeons Run Level Range": {
+            
+        },	
+
+        "Cheese Section": {
+            
+        },		
+
+        "Ultimate Enchants": {
+            
+        },		
+
+        "General FAQs": {
+            
+        },		
+        
+        "Update Tips": {
+            
+        },	
+        
+    },
+
+    
+    // BLOCK TEXT
+
+
+    "Table of Contents:": {
+
+    }
+}
