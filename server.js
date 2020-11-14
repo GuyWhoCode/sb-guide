a12ab9fe1798a5e0f74ab811b1824c22c64f398a
= <jasonagus@Jasons-MacBook-Air.local>
2020-11-13 20:39:32 -0800

    Fixed typo

--- server.js
+++ server.js
@@ -7,17 +7,11 @@ client.once('ready', () => {
   console.log("Ready!")
 })
 
-// client.on('message', messsage => {
-//   //.startsWith(`${prefix}start`)
-//   if (message.content === "g!start") {
-//     message.content.send("Bot has started!")
-//   }
-// })
 client.on('message', message => {
-	if (message.content === '!ping') {
-		message.channel.send('Pong.');
-	}
-});
+  if (message.content.startsWith(`${prefix}start`)) {
+    message.content.send("Bot has started!")
+  }
+})
 
 client.login(process.env.botToken)
 
