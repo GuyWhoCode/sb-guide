5d2103742c31bd15f22b7e34b4f688e3810d291f
= <jasonagus@Jasons-MacBook-Air.local>
2020-11-13 20:46:32 -0800

    double no semi

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ client.on('message', message => {
   //   message.content.send("Bot has started!")
   // }
   if (message.content.startsWith(`${prefix}ping`)) {
-    message.channel.send('Pong.');
+    message.channel.send("Pong.")
   } else if (message.content.startsWith(`${prefix}beep`)) {
-    message.channel.send('Boop.');
+    message.channel.send("Boop.")
   }
 })
 
