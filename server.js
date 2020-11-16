1434782273dc8fa0d0a5f657a3ddb36971bf5465
= <jasonagus@Jasons-MacBook-Air.local>
2020-11-16 15:10:30 -0800

    Imported MongoDB

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
 const prefix = 'g!'
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:<password>@scoutingapp.pblik.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+// client.connect( async(err, client)=> {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 
+//Reload Command
 client.once('ready', () => {
   console.log("Ready!")
 })
@@ -11,6 +21,7 @@ client.on('message', message => {
   if (message.content.startsWith(`${prefix}start`)) {
     message.channel.send("Bot has started!")
   }
+  // message.guild.channels
 })
 
 client.login(process.env.botToken)
