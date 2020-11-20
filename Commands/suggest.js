c21087e77aa7ede2af2eb2013d9f36b6ea9edb60
= <jasonagus@Jasons-MacBook-Air.local>
2020-11-20 11:49:17 -0800

    final embed

--- Commands/suggest.js
+++ Commands/suggest.js
@@ -15,37 +15,34 @@ const suggestionSchema = {
 //   smth.user = pingUser(914534857345)
 // }
 
-
+var suggestEmbed = {
+  color: 0xffba00,
+  title: 'Suggestion',
+  description: "Filler Suggestion",
+  timestamp: new Date(),
+  footer: {
+    text: 'Skycomm Guide Bot',
+    icon_url: "https://i.imgur.com/184jyne.png",
+  },
+}
 
 
 module.exports = {
 	name: 'suggest',
 	description: "Adds a suggestion to update the Skyblock or Dungeons guide.",
 	execute(message, args) {
-    var suggestEmbed = {
-      color: 0xffba00,
-      title: 'Suggestion',
-      description: "Filler Suggestion",
-      timestamp: new Date(),
-      footer: {
-        text: 'Skycomm Guide Bot',
-        icon_url: "https://i.imgur.com/184jyne.png",
-      },
-    }
-
     let userSuggestion = args.slice(1, args.length).join(" ")
     suggestEmbed.description = userSuggestion
 
-    //  suggestEmbed.description = args
-    // let user = message.author.username
+    let user = message.author.username
 
-    // if (args[1] == "sb") {
-    //   suggestEmbed.title = `Skyblock Guide Suggestion made by ME`
-    // } else if (args[1] == "d") {
-    //   suggestEmbed.title = `Dungeons Suggestion made by ME`
-    // }
+    if (args[0] == "sb") {
+      suggestEmbed.title = `Skyblock Guide Suggestion made by ${user}`
+    } else if (args[0] == "d") {
+      suggestEmbed.title = `Dungeons Suggestion made by ${user}`
+    }
     
-    message.channel.send({ embed: suggestEmbed })
-    // message.channel.send("These is the user suggestion! " + userSuggestion)
+    let suggestionChannel = message.guild.channels.cache.find(ch => ch.name === "suggested-guide-changes")
+	  suggestionChannel.send({ embed: suggestEmbed })
 	},
 }
