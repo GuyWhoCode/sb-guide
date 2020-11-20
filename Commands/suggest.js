aa064da2f787a2d3232761690ac03a02e2a18269
= <jasonagus@Jasons-MacBook-Air.local>
2020-11-20 15:05:49 -0800

    Debug #7

--- Commands/suggest.js
+++ Commands/suggest.js
@@ -47,14 +47,15 @@ module.exports = {
     //   return;
     // }
 
-    if (args[0] == "sb") {
+    if (args[1] == "sb") {
       suggestEmbed.title = `Skyblock Guide Suggestion made by ${user}`
-    } else if (args[0] == "d") {
+    } else if (args[1] == "d") {
       suggestEmbed.title = `Dungeons Guide Suggestion made by ${user}`
     } 
     
     // suggestEmbed.fields[0].name = `ID: ${message.id}`
     let suggestionChannel = message.guild.channels.cache.find(ch => ch.name === "suggested-guide-changes")
-	  suggestionChannel.send({ embed: suggestEmbed })
+    suggestionChannel.send({ embed: suggestEmbed })
+    message.channel.send("This command works! Args: " + args[1])
 	},
 }
