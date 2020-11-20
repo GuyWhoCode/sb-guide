c95541bb93144578eb79c257311cdf8cdb402d4a
= <jasonagus@Jasons-MacBook-Air.local>
2020-11-20 11:21:20 -0800

    now accounts for sb/d

--- Commands/suggest.js
+++ Commands/suggest.js
@@ -34,9 +34,16 @@ module.exports = {
     let userSuggestion = args.split(2, args.length).join(" ")
     suggestEmbed.description = userSuggestion
 
-    // let user = message.author.username
-    // suggestEmbed.title = `Suggestion made by ${user}`
+    let user = message.author.username
 
+    if (args[1] == "sb") {
+      suggestEmbed.title = `Skyblock Guide Suggestion made by ${user}`
+    } else if (args[1] == "d") {
+      suggestEmbed.title = `Dungeons Suggestion made by ${user}`
+    }
+    
+
+    
     message.channel.send({ embed: suggestEmbed })
 	},
 }
