6cd62fb55ac8d708c799ec85bdc621ea0cd1075a
= <jasonagus@Jasons-MacBook-Air.local>
2020-11-22 07:13:26 -0800

    another another locked role test

--- server.js
+++ server.js
@@ -60,11 +60,8 @@ client.on('message', (message) => {
 
 	if (message.channel.name != "guide-discussion" && message.channel.name != "bot-commands") return message.channel.send("Wrong channel. Please use <#772948480972161044> or <#587815634641879076>!")
 	
-	// let lockedRole = message.guild.roles.find("name", "Guide Locked")
-	// if (message.channel.guild(779947579248214047)) return message.channel.send("You have been locked from suggesting anything.")
-
-	message.content.send("Roles that user has: " + message.channel.guild(779947579248214047))
-	// const args = message.content.slice(prefix.length).trim().split(/ +/);
+	if (message.member.roles.find(role => role.name == "Guide Locked")) return message.channel.send("You have been locked from suggesting anything.")
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
 
 	message.channel.send("Args: " + args + "\nCommand: " + command)
