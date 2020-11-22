df2892f2a1897e3cea8612a8da77ab84321724c6
= <jasonagus@Jasons-MacBook-Air.local>
2020-11-21 17:20:12 -0800

    spelled args wrong

--- Commands/reload.js
+++ Commands/reload.js
@@ -4,7 +4,7 @@ module.exports = {
     description: "Reloads a command for testing.",
     execute(message, args) {
         if (args.length == 0) return message.channel.send("There is no command to reload!")
-        let commandName = agrs[0].toLowerCase()
+        let commandName = args[0].toLowerCase()
         const command = message.client.commands.get(commandName) || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
         if (!command) return message.channel.send(`There is no command with name or alias ${commandName}`)
