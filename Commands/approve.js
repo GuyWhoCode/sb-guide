42ddb73419f725d88f39e693ba4d48940b966b23
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-11-24 20:46:31 -0800

    testing database

--- Commands/approve.js
+++ Commands/approve.js
@@ -1,7 +1,7 @@
 // Parameters: (MESSAGE ID, guide Name (SB / Dungeons), Category name)
-// const mongoClient = require('mongodb').MongoClient
-// const uri = "mongodb+srv://dbADMIN:"+ process.env.password + "@guide-info.e5dr4.mongodb.net/skyblockGuide?retryWrites=true&w=majority";
-// const dbClient = new mongoClient(uri, { useNewUrlParser: true })
+const mongoClient = require('mongodb').MongoClient
+const uri = "mongodb+srv://dbADMIN:"+ process.env.password + "@guide-info.e5dr4.mongodb.net/skyblockGuide?retryWrites=true&w=majority";
+const dbClient = new mongoClient(uri, { useNewUrlParser: true })
 
 module.exports = {
 	name: 'approve',
@@ -15,15 +15,22 @@ module.exports = {
 		// var sectionTitle = args[2] 		
 		// if (category != "sb" && category != "d") return message.channel.send("You did not specify the right category. Please use the right format. `g!approve [category] [suggestion ID] [Category Name]`")
 		
-		// if (messageID.length != 18) return message.channel.send("The given message ID was copied wrong. Please use the right format. `g!approve [category] [suggestion ID] [Category Name]`")
-		let suggestionChannel = message.guild.channels.cache.find(ch => ch.name === "suggested-guide-changes")
-		suggestionChannel.messages.fetch({around: messageID, limit: 1})
-			.then(msg => {
-			  msg.first().edit("This fetched message was edited");
-			})
+		// if (messageID.length != 18) 
+		// let suggestionChannel = message.guild.channels.cache.find(ch => ch.name === "suggested-guide-changes")
+		// suggestionChannel.messages.fetch({around: messageID, limit: 1})
+		// 	.then(msg => {
+		// 	  msg.first().edit("This fetched message was edited");
+		// 	})
 
 		// if () return message.channel.send("The given message ID was copied wrong. Please use the right format. `g!approve [category] [suggestion ID] [Category Name]`")
+		dbClient.connect( async(err) => {
+			let suggestionDB = dbClient.db("skyblockGuide").collection("suggestions")
+			let suggestion = await suggestionDB.findOne({"messageID": messageID}).toArray()
 
+			if (suggestion.length == 0) return message.channel.send("The given message ID was copied wrong. Please use the right format. `g!approve [category] [suggestion ID] [Category Name]`")
+
+			message.channel.send("This is what I got back from my database! Suggestion: " + suggestion[0].description)
+		})
 
 		message.channel.send('Approve command!')
 	},
