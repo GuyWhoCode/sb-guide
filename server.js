0c10ade8dd21e6ca5d68e7b34cefa9e711ee6993
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-11-25 17:44:01 -0800

    no more debug message

--- server.js
+++ server.js
@@ -69,8 +69,6 @@ client.on('message', (message) => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	message.channel.send("Args: " + args + "\nCommand: " + command)
-
 	try {
 		client.commands.get(command).execute(message, args)
 	} catch (error) {
