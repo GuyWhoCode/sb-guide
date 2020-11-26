cd844c94c8aaf32b790309aa3476fd5197694b1d
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-11-26 15:37:14 -0800

    skeleton delete command

--- Commands/delete.js
+++ Commands/delete.js
@@ -4,12 +4,15 @@ module.exports = {
 	description: 'Deletes a suggestion or a section from the guide.',
 	execute(message, args) {
 		if (args.length == 0) return message.channel.send("You need to input a Message ID! See `g!delete <Message ID>`")
-		
+		let messageID = args[0]
 		let suggestionChannel = message.guild.channels.cache.find(ch => ch.name === "suggested-guide-changes")
+		
 		suggestionChannel.messages.fetch({around: messageID, limit: 1})
 			.then(msg => {
-			  msg.first().edit("This fetched message was edited");
+			  msg.first().edit("This fetched message was edited")
 			})
+			.catch(message.channel.send("There was no message with that Message ID"))
+		
 		message.channel.send('Delete command!!')
 	},
 
