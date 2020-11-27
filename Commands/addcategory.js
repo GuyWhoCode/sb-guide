32a62c50e2f01805fed462a2a7add3b1380cde45
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-11-26 18:38:58 -0800

    replaced outdated msg

--- Commands/addcategory.js
+++ Commands/addcategory.js
@@ -1,7 +1,7 @@
 var categoryEmbed = {
   color: 0xffba00,
   title: 'Suggestion',
-  description: "This is an empty section of the guide. Add some changes to this guide using `g!suggest [category] [Suggestion]` !",
+  description: "This is an empty section of the guide. Add some changes to this guide using!",
   fields: [
 		{
 			name: '_ _',
@@ -32,10 +32,12 @@ module.exports = {
         category = "Skyblock"
         categoryChannel = message.guild.channels.cache.find(ch => ch.name === "skyblock-guide")
         categoryEmbed.color = 0x87d8fa
+        categoryEmbed.description = "This is an empty section of the guide. Add some changes to this guide using `g!sbsuggest <Suggestion>`!"
       } else if (category == "d") {
         category = "Dungeons"
         categoryChannel = message.guild.channels.cache.find(ch => ch.name === "dungeons-guide-n-tips")
         categoryEmbed.color = 0xcc0000
+        categoryEmbed.description = "This is an empty section of the guide. Add some changes to this guide using `g!dsuggest <Suggestion>`!"
       } else if (category == "u") {
         category = "Update Tips"
         
