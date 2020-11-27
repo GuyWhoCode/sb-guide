f2958f2a5be8cb8154f3cf37696d1f36f41b17b0
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-11-26 16:10:55 -0800

    crappy whole if for specific role

--- Commands/delete.js
+++ Commands/delete.js
@@ -6,23 +6,25 @@ module.exports = {
 	name: 'delete',
 	description: 'Deletes a suggestion or a section from the guide.',
 	execute(message, args) {
-		// if (args.length == 0) return message.channel.send("You need to input a Message ID! See `g!delete <Message ID>`")
-		if (message.member.roles.cache.find(role => role.name != "Discord Staff") && message.member.roles.cache.find(role => role.name != "Discord Management")) return message.channel.send("You don't have permission to use this command!")
-		let messageID = args[0]
-		let suggestionChannel = message.guild.channels.cache.find(ch => ch.name === "suggested-guide-changes")
-		
-		suggestionChannel.messages.fetch({around: messageID, limit: 1})
-			.then(msg => {
-			  msg.first().delete()
-			  message.channel.send("Suggestion found and deleted.")
+		if (args.length == 0) return message.channel.send("You need to input a Message ID! See `g!delete <Message ID>`")
+		if (message.member.roles.cache.find(role => role.name == "Discord Staff") && message.member.roles.cache.find(role => role.name == "Discord Management")) {
+			let messageID = args[0]
+			let suggestionChannel = message.guild.channels.cache.find(ch => ch.name === "suggested-guide-changes")
+			
+			suggestionChannel.messages.fetch({around: messageID, limit: 1})
+				.then(msg => {
+				  msg.first().delete()
+				  message.channel.send("Suggestion found and deleted.")
+				})
+			
+			dbClient.connect(async(err) => {
+				let suggestionDB = dbClient.db("skyblockGuide").collection("suggestions")
+				await suggestionDB.deleteOne({"messageID": messageID})
 			})
+		} else {
+			message.channel.send("You don't have permission to use this command!")
+		}
 		
-		dbClient.connect(async(err) => {
-			let suggestionDB = dbClient.db("skyblockGuide").collection("suggestions")
-			await suggestionDB.deleteOne({"messageID": messageID})
-		})
-		
-		message.channel.send('Delete command!!')
 	},
 
 }
