519c599c5c88c78074c874c3a26241e6fc7dfc7c
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-11-27 07:19:47 -0800

    added db to addcategory

--- Commands/addcategory.js
+++ Commands/addcategory.js
@@ -1,3 +1,17 @@
+const categorySchema = {
+  "embedMessage": {},
+  "categoryTitle": "placeholder",
+  "messageID": "placeholder"
+}
+
+const makeNewEntry = (msg, title, id) => {
+  let entry = Object.create(categorySchema)
+  entry.embedMessage = msg
+  entry.categoryTitle = title
+  entry.messageID = id
+  return entry
+}
+
 var categoryEmbed = {
   color: 0xffba00,
   title: 'Suggestion',
@@ -18,16 +32,17 @@ module.exports = {
     name: "addcategory",
     description: "Adds a new category to <#772942075301068820> or <#772944394542121031>",
     execute(message, args) {
+      var categoryChannel = ""
+      var msgID = ""
       var category = args[0]
       if (category != "sb" && category != "d") return message.channel.send("You are missing an argument! Please use the right format. `g!addcategory [category] [Category Name]`")
       
       let categoryName = args.slice(1, args.length).join(" ").trim()
       if (categoryName.length == 0) return message.channel.send("You need to input a Category Name! See `g!addcategory  [Category Name]`")
       
-      var categoryChannel = ""
 
       categoryEmbed.title = categoryName
-        
+  
       if (category == "sb") {
         category = "Skyblock"
         categoryChannel = message.guild.channels.cache.find(ch => ch.name === "skyblock-guide")
@@ -43,14 +58,16 @@ module.exports = {
         
       }
       
-      categoryChannel.send({ embed: categoryEmbed })
-      // dbClient.connect( async(err)=> {
-      //   let database = dbClient.db("skyblockGuide")
-      //   let categoryDB = database.collection(category)
-        
+      categoryChannel.send({ embed: categoryEmbed }).then(msg => msgID = msg.id)
 
-      //   suggestionsDB.insertOne(newEntry)
-      // })
+      dbClient.connect( async(err)=> {
+        let database = dbClient.db("skyblockGuide")
+        let categoryDB = database.collection(category)
+        
+        let newEntry = makeNewEntry(categoryEmbed, categoryName, msgID)
+        categoryDB.insertOne(newEntry)
+      })
+
       message.channel.send("Your category has been created!")
       }
 }
