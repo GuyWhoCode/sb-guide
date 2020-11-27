c77dcb7ab1a1e0e322a0785f5acf9b3972c8b41d
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-11-26 16:42:02 -0800

    formatting approve.js

--- Commands/approve.js
+++ Commands/approve.js
@@ -7,7 +7,7 @@ module.exports = {
 	name: 'approve',
 	description: 'Approves a suggestion.',
 	execute(message, args) {
-		// if (args.length != 1) return message.channel.send("Please use the right format. `g!approve [category] [suggestion ID] [Category Name]`")
+		// if (args.length != 1) return message.channel.send("Please use the right format. `g!approve <Category> <Suggestion ID> <Category Name>`")
 		//Weeds out all bad commands
 
 		// var category = args[0]
@@ -15,11 +15,7 @@ module.exports = {
 		// var sectionTitle = args[2] 		
 		// if (category != "sb" && category != "d") return message.channel.send("You did not specify the right category. Please use the right format. `g!approve [category] [suggestion ID] [Category Name]`")
 		
-		// let suggestionChannel = message.guild.channels.cache.find(ch => ch.name === "suggested-guide-changes")
-		// suggestionChannel.messages.fetch({around: messageID, limit: 1})
-		// 	.then(msg => {
-		// 	  msg.first().edit("This fetched message was edited");
-		// 	})
+
 
 		dbClient.connect( async(err) => {
 			let suggestionDB = dbClient.db("skyblockGuide").collection("suggestions")
@@ -27,6 +23,11 @@ module.exports = {
 
 			if (suggestion.length == 0) return message.channel.send("The given message ID was copied wrong. Please use the right format. `g!approve [category] [suggestion ID] [Category Name]`")
 
+					// let suggestionChannel = message.guild.channels.cache.find(ch => ch.name === "suggested-guide-changes")
+		// suggestionChannel.messages.fetch({around: messageID, limit: 1})
+		// 	.then(msg => {
+		// 	  msg.first().edit("This fetched message was edited");
+		// 	})
 			message.channel.send("This is what I got back from my database! Suggestion: " + suggestion[0].description)
 		})
 
