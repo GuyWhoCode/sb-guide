f117ba9a32b82f3e45eca693048bf15f8feee205
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-11-27 07:20:59 -0800

    forgot to add db

--- Commands/addcategory.js
+++ Commands/addcategory.js
@@ -1,3 +1,7 @@
+const mongoClient = require('mongodb').MongoClient
+const uri = "mongodb+srv://dbADMIN:"+ process.env.password + "@guide-info.e5dr4.mongodb.net/skyblockGuide?retryWrites=true&w=majority";
+const dbClient = new mongoClient(uri, { useNewUrlParser: true })
+
 const categorySchema = {
   "embedMessage": {},
   "categoryTitle": "placeholder",
