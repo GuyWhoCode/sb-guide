ee101566dedd909e44d9affc74a5391208c740b4
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-12-05 11:05:52 -0800

    15 second delay for redirect message

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ client.on('message', (message) => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	//weeds out messages that don't start with the prefix and the author of the message is a bot.
 
-	if (message.channel.name != "guide-discussion" && message.channel.name != "bot-testing" && message.channel.name != "bot-commands") return message.reply("Wrong channel. Please use <#772948480972161044> or <#587815634641879076>!").then(msg => msg.delete({ timeout: 5000}))
+	if (message.channel.name != "guide-discussion" && message.channel.name != "bot-testing" && message.channel.name != "bot-commands") return message.reply("Wrong channel. Please use <#772948480972161044> or <#587815634641879076>!").then(msg => msg.delete({ timeout: 15000}))
 	//weeds out messages that aren't in the proper channel.
 
 	if (message.member.roles.cache.find(role => role.name == "Guide Locked")) return message.channel.send("You have been locked from suggesting anything.")
