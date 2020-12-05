8729c3358f7caa8432e57803fbc51c3a58d7f5c9
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-12-04 21:40:59 -0800

    pulling embed msg

--- Commands/update.js
+++ Commands/update.js
@@ -1,3 +1,7 @@
+const mongoClient = require('mongodb').MongoClient
+const uri = "mongodb+srv://dbADMIN:"+ process.env.password + "@guide-info.e5dr4.mongodb.net/skyblockGuide?retryWrites=true&w=majority";
+const dbClient = new mongoClient(uri, { useNewUrlParser: true })
+
 module.exports = {
     name: "update",
     description: "The latest and greatest for updates coming out!",
@@ -5,8 +9,15 @@ module.exports = {
         if (args.length == 0) return message.channel.send("You need to input something! See `g!update <Update Tip>`")
         
         let updateSuggestion = args.join(" ").trim()
-        
-        
+
+        dbClient.connect(async (err) => {
+            let updateDB = dbClient.db("skyblockGuide").collection("Update Tips")
+            let findUpdateMsg = await updateDB.findOne({"identifier": "Update Tips"}).toArray()
+
+            let updateMsg = findUpdateMsg[0].msgObject
+            message.channel.send({embed: updateMsg})
+        })
+
         message.channel.send("User output: " + updateSuggestion)
     }
 }
