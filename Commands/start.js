0ac5ddc2f8ce5e134b6250a18dfc86b0f25cd302
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-12-06 07:49:17 -0800

    raw string ouput channel

--- Commands/start.js
+++ Commands/start.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'start',
 	description: 'Starts up!',
 	execute(message, args) {
-		message.channel.send('Bot has started! With these args: ' + args.join(" ").trim())
+		message.channel.send('Bot has started! With these args: ' + "`" + args.join(" ").trim() + "`")
 	},
 }
