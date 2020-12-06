79c5cb4cc461a1f47f1bbda1a14cfe3c036024a5
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-12-05 16:42:09 -0800

    tried require for sbsuggest

---
+++ mongodb.js
@@ -0,0 +1,7 @@
+const mongoClient = require('mongodb').MongoClient
+const uri = "mongodb+srv://dbADMIN:"+ process.env.password + "@guide-info.e5dr4.mongodb.net/skyblockGuide?retryWrites=true&w=majority";
+const dbClient = new mongoClient(uri, { useNewUrlParser: true })
+
+module.exports = {
+    dbClient: dbClient
+}
