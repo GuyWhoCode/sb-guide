29969f5a319207eccd61f9202d319b65f33e254a
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-12-05 16:47:30 -0800

    wrong file path?

--- Commands/sbsuggest.js
+++ Commands/sbsuggest.js
@@ -1,4 +1,4 @@
-const {dbClient} = require("./mongodb.js")
+const {dbClient} = require("../mongodb.js")
 // const mongoClient = require('mongodb').MongoClient
 // const uri = "mongodb+srv://dbADMIN:"+ process.env.password + "@guide-info.e5dr4.mongodb.net/skyblockGuide?retryWrites=true&w=majority";
 // const dbClient = new mongoClient(uri, { useNewUrlParser: true })
