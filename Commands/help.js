25c285f93cd0919772219aa5e45513b3f28a8161
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-12-06 15:13:45 -0800

    Complete help command

--- Commands/help.js
+++ Commands/help.js
@@ -11,7 +11,7 @@ var helpEmbed = {
 			value: 'Adds your suggestion to <#772944441643630602> to be approved by a Contributor.',		
 		},
 		{
-			name: 'Dungeons Guide Suggestion `g!dsuggestion <Suggestion>`',
+			name: 'Dungeons Guide Suggestion `g!dsuggest <Suggestion>`',
 			value: 'Adds your suggestion to <#772944441643630602> to be approved by a Contributor.',
 		},
 		{
@@ -19,22 +19,34 @@ var helpEmbed = {
 			value: 'Post tips in <#779467383604772874> whenever a new Skyblock Update is out.',
 		},
 		{
+			name: 'List Categories `g!listcategories <Category>`',
+			value: 'Lists all of the Guide Categories available based on the given general category. See Add Category for Category Alises.',
+		},
+		{
+			name: 'Search `g!search <Query>`',
+			value: 'Searches the guides that best match your query. Recommend to do `g!listcategories <Category>` first to narrow search.',
+		},
+		{
 			name: 'Add Category `g!addcategory <Category> <Category Name>`',
 			value: 'Adds an embed for people to add changes in their respective channels.\n<#772942075301068820> Category Alises: `sb, skyblock, Skyblock, SB, SkyBlock`\n<#772944394542121031> Category Alises: `d, dungeons, dung, Dungeons, D, dungeon, Dungeon, Dung`\n<#779467383604772874> Category Alises: `u, update, U, Update, UPDATE`',
-		}
-		// ,
-		// {
-		// 	name: ' `g!`',
-		// 	value: '',
-		// },
-		// {
-		// 	name: ' `g!`',
-		// 	value: '',
-		// },
-		// {
-		// 	name: ' `g!`',
-		// 	value: '',
-		// },
+		},
+		{
+			name: 'Add Section `g!addsection <Category> <Message ID>`',
+			value: 'Adds a new subtitle to a Category Message. See Category alises above.',
+		},
+		{
+			name: 'Approve `g!approve <Suggestion Message ID>`',
+			value: 'Approves any suggestion made in <#772944441643630602> and edits the message in the Skyblock or Dungeons Guide.',
+		},
+		{
+			name: 'Delete `g!delete <Message ID> <#Channel>`',
+			value: 'Deletes suggestions from <#772944441643630602> and deletes specific sections from any of the Guides.',
+		},
+		{
+			name: 'Edit `g!edit <Message ID> <#Channel>`',
+			value: 'Edits a Section of the Guide based on the Message ID supplied.',
+		},
+
 	],
 	timestamp: new Date(),
 	footer: {
