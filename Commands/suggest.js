fe0ac699629576eae19ecf124e8415df28cde6a0
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-12-09 10:29:25 -0800

    spelled commands wrong

--- Commands/suggest.js
+++ Commands/suggest.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "suggest",
     execute(message, args) {
-        message.channel.send("This command has been removed due to not being user friendly. Please use the following commmands as a substitute.\n`g!sbsuggest <Suggestion>`- Skyblock Guide Suggestion\n`g!dsuggest <Suggestion>` - Dungeon Guide Suggestion")
+        message.channel.send("This command has been removed due to not being user friendly. Please use the following commands as a substitute.\n`g!sbsuggest <Suggestion>`- Skyblock Guide Suggestion\n`g!dsuggest <Suggestion>` - Dungeon Guide Suggestion")
     }
 }
