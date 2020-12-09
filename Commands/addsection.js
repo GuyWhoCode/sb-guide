e9f024df7c50abc68c155319b1069590a6192773
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-12-08 18:37:45 -0800

    checking the wrong thing?

--- Commands/addsection.js
+++ Commands/addsection.js
@@ -20,9 +20,9 @@ module.exports = {
 		dbClient.connect(async (err) => {
 			let guideCollection = dbClient.db("skyblockGuide").collection("Guides")
             let categoryMsg = await guideCollection.find({"categoryTitle": categoryName}).toArray()
-            if (categoryMsg[0] == undefined) return message.channel.send("The Category Name provided did not match anything. Did you type it wrong?")
             
             let msgEmbed = categoryMsg[0].embedMessage
+            if (msgEmbed == undefined) return message.channel.send("The Category Name provided did not match anything. Did you type it wrong?")
             
             var newEntry = Object.create(entrySchema)
             newEntry.name = sectionName
