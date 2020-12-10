4fb09a86c64eb3ccffb3fcc45b837d3e0df36a4f
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-12-10 15:07:01 -0800

    copy paste

--- Commands/addsection.js
+++ Commands/addsection.js
@@ -19,7 +19,7 @@ module.exports = {
     description: "Adds a section to either a Skyblock Guide or a Dungeons Guide",
     execute(message, args) {
         if (args.length == 0) return message.channel.send("`g!addsection <Category-Name> <Section Name>`")
-        if (args.length >= 4) return message.channel.send("I received more parameters (>3) than I can work with. If there are more than 2 words in the Category name, please replace the space with a hyphen (-), but keep the Capitalization. It's CaSe SeNsItIvE")
+        if (args.length >= 3) return message.channel.send("I received more parameters (>3) than I can work with. If there are more than 2 words in the Category name, please replace the space with a hyphen (-), but keep the Capitalization. It's CaSe SeNsItIvE.\nEx. My-Category-Name")
 
         var categoryName = translateCategoryName(args[0])
         if (categoryName.length == 0) return message.channel.send("You need to input a Category name from the Skyblock/Dungeons Guide! See `g!addsection <Category-Name> <Section Name>`")
