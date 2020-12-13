11dbc2e03233c30fe611c5e8977a02a9eba1c86e
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-12-12 19:41:38 -0800

    2 edge cases added

--- Commands/approve.js
+++ Commands/approve.js
@@ -10,6 +10,9 @@ const translateCategoryName = name => {
     }
 }
 
+const capitalizeString = str => {
+	return str[0].toUpperCase() + str.substring(1)
+}
 module.exports = {
 	name: 'approve',
 	description: 'Approves a suggestion.',
@@ -31,16 +34,16 @@ module.exports = {
 			if (suggestion[0].status === "Approved") return message.channel.send("The suggestion was already approved!")
 
 			let categoryMsg = await guidesDB.find({"categoryTitle": categoryTitle}).toArray()
-			// let embedMessage = categoryMsg[0].embedMessage
+			let embedMessage = categoryMsg[0].embedMessage
 			if (categoryMsg[0] == undefined) return message.channel.send("The Category Title that was given was incorrect. Remember to separate Category titles with more than 2 words with hyphens. It is CaSe SeNsItIvE.")
 			
-			return message.channel.send("Nothing crashed up to this!")
-			//categoryMsg[0]
 			var foundSection = false
 			embedMessage.fields.map(val => {
 				val.name === sectionTitle ? (val.value === "_ _" ? val.value = suggestion[0].description + "\n\u200b": val.value += "\n\u200b" + suggestion[0].description + "\n\u200b", foundSection = true): undefined
 			})
 			if (foundSection == false) return message.channel.send("The section that was given was incorrect. Remember to separate Section titles with more than 2 words with hyphens. It is CaSe SeNsItIvE.")
+
+			if (suggestion[0].section === categoryMsg[0].category || capitalizeString(suggestion[0].section) === categoryMsg[0].category) return message.channel.send("The suggestion that you have tried to approve does not match with the category's guide. Make sure that Skyblock Suggestions are approved for the Skyblock Guide and that Dungeon Suggestions are approved for the Dungeons Guide")
 
 
 			let suggestionChannel = message.guild.channels.cache.find(ch => ch.name === "suggested-guide-changes")
@@ -49,19 +52,17 @@ module.exports = {
 				  msg.first().edit("This suggestion has been approved!");
 				})
 
+			var guideMessage = ""
 			if (categoryMsg[0].category === "Skyblock") {
-				let guideMessage = message.guild.channels.cache.find(ch => ch.name === "skyblock-guide")
-				guideMessage.messages.fetch({around: messageID, limit: 1})
-				.then(msg => {
-				  msg.first().edit({embed: embedMessage});
-				})
+				guideMessage = message.guild.channels.cache.find(ch => ch.name === "skyblock-guide")
 			} else if (categoryMsg[0].category === "Dungeons") {
-				let guideMessage = message.guild.channels.cache.find(ch => ch.name === "dungeons-guide-n-tips")
-				guideMessage.messages.fetch({around: messageID, limit: 1})
+				guideMessage = message.guild.channels.cache.find(ch => ch.name === "dungeons-guide-n-tips")
+			}
+			
+			guideMessage.messages.fetch({around: messageID, limit: 1})
 				.then(msg => {
 				  msg.first().edit({embed: embedMessage});
 				})
-			}
 
 			suggestionDB.updateOne({"messageID": messageID}, {$set: {"section": suggestion[0].section, "messageID": messageID, "description": suggestion[0].description, "user": suggestion[0].user, "status": "Approved"}})
 			guidesDB.updateOne({"categoryTitle": categoryTitle}, {$set: {"embedMessage": embedMessage, "categoryTitle": categoryMsg[0].categoryTitle, "messageID": categoryMsg[0].messageID, "category": categoryMsg[0].category}})
