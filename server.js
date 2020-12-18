5a8f506b27979e5e07ba5dbbfce46178e9617b59
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-12-17 16:15:36 -0800

    seeing array

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ client.on('message', (message) => {
 		args.unshift(command.split("\n")[1])
 	}
 
-	message.channel.send(command.split("\n")[1])
+	message.channel.send(command.split("\n"))
 	try {
 		let userCmd = client.commands.get(command) || client.commands.find(cmd => cmd.alises && cmd.alises.includes(command))
 		userCmd.execute(message, args)
