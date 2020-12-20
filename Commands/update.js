70e2682c6201d4be940176d3fa5489abb639d3a2
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-12-20 13:23:01 -0800

    ad command

--- Commands/update.js
+++ Commands/update.js
@@ -6,7 +6,6 @@ const entrySchema = {
 }
 module.exports = {
     name: "update",
-    description: "The latest and greatest for updates coming out!",
     alises: ['u', 'Update'],
     execute(message, args) {
         if (args.length == 0) return message.channel.send("You need to input something! See `g!update <Update Tip>`")
