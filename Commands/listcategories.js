be49d52004231b860d7575a7e1631e309eda76c6
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-12-22 13:57:51 -0800

    hot links

--- Commands/listcategories.js
+++ Commands/listcategories.js
@@ -7,22 +7,24 @@ module.exports = {
 	alises: ["lc", "list", "listc", "listC", "Listcategories", "listcategory", "Listcategory"],
 	execute(message, args) {
 		var guide = args[0]
+		var categoryID = ""
 		if (args.length == 0 || guide == undefined) return message.channel.send('See `g!listcategories <Section>`')
 		//checks if there is any bad input
 		
 		if (globalFunctions.checkAliases(sbAlias, guide) == false && globalFunctions.checkAliases(dAlias, guide) == false) return message.channel.send('You are missing an argument! See `g!listcategories <Guide>`')
 		//checks if provided Guide matches alias list
 
-		if (globalFunctions.checkAliases(sbAlias, guide)) guide = "Skyblock"
-		if (globalFunctions.checkAliases(dAlias, guide)) guide = "Dungeons"
+		if (globalFunctions.checkAliases(sbAlias, guide)) guide = "Skyblock", categoryID = "772942075301068820"
+		if (globalFunctions.checkAliases(dAlias, guide)) guide = "Dungeons", categoryID = "772944394542121031"
 
 		dbClient.connect(async (err) => {
 			let categoryCollection = dbClient.db("skyblockGuide").collection("Guides")
 			var categoryList = await categoryCollection.find({"category": guide}).toArray()
-			var categoryMsg = ""
+			categoryList.map(val => categoryMsg += "`" + val.categoryTitle + "`" + `https://discord.com/channels/587765474297905158/${channelID}/${val.messageID}` + "\n")
 
-			categoryList.map(val => categoryMsg += "`" + val.categoryTitle + "`" + "\n")
 			message.channel.send("List of categories for " + guide + ":\n" + categoryMsg)
+			// - Skyblock Channel ID
+			//https://discord.com/channels/Server ID/Channel ID/Message ID
 		})
 	},
 }
