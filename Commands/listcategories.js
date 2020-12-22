b531dedc6e3a1056577c00a8487ce7077a02cc6e
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-12-22 14:54:38 -0800

    only in value field

--- Commands/listcategories.js
+++ Commands/listcategories.js
@@ -41,7 +41,7 @@ module.exports = {
 			let categoryCollection = dbClient.db("skyblockGuide").collection("Guides")
 			var categoryList = await categoryCollection.find({"category": guide}).toArray()
 			
-			categoryList.map(val => listEmbed.fields.push({name: "[" + val.categoryTitle + "](" + makeMsgLink(val.messageID) + ")", value: "_ _"}))
+			categoryList.map(val => listEmbed.fields.push({name: val.categoryTitle, value: "[Jump](" + makeMsgLink(val.messageID) + ")"}))
 
 			listEmbed.timestamp = new Date()
 			listEmbed.title = "List of categories for " + guide
