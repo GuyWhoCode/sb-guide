ad2cdea47e0bb52d283fed540e05819d1ea519cb
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-12-27 20:22:53 -0800

    testing regex expression

--- Commands/post.js
+++ Commands/post.js
@@ -7,23 +7,25 @@ module.exports = {
     execute(message, args) {
         if (args.length == 0 || args[0] == undefined) return message.channel.send("See `g!post <Category-Name>`")
         //checks if there is any bad input
-        var categoryName = globalFunctions.translateCategoryName(args[0])
+        var categoryName = "/" + globalFunctions.translateCategoryName(args[0]) + "/gi"
+
         dbClient.connect(async (err) => {
             let guidesDB = dbClient.db("skyblockGuide").collection("Guides")
-            let guide = await guidesDB.find({"categoryTitle": categoryName}).toArray()
+            let guide = await guidesDB.find({"categoryTitle": {$regex: categoryName}}).toArray()
             
             let guideMessage = guide[0].embedMessage
             guideMessage.timestamp = new Date()
 
-            var guideChannel = ""
-            guide[0].category === "Skyblock" ? guideChannel = message.guild.channels.cache.find(ch => ch.name === "skyblock-guide") : guideChannel = message.guild.channels.cache.find(ch => ch.name === "dungeons-guide-n-tips")
+            message.channel.send(guideMessage)
+            // var guideChannel = ""
+            // guide[0].category === "Skyblock" ? guideChannel = message.guild.channels.cache.find(ch => ch.name === "skyblock-guide") : guideChannel = message.guild.channels.cache.find(ch => ch.name === "dungeons-guide-n-tips")
 			
-            guideChannel.send({embed: guideMessage}).then(msg => {
-				newMsgId = msg.id
-				guidesDB.updateOne({"categoryTitle": categoryName}, {$set: {"embedMessage": guideMessage, "categoryTitle": guide[0].categoryTitle, "messageID": newMsgId, "category": guide[0].category}})
-			})
+            // guideChannel.send({embed: guideMessage}).then(msg => {
+			// 	newMsgId = msg.id
+			// 	guidesDB.updateOne({"categoryTitle": categoryName}, {$set: {"embedMessage": guideMessage, "categoryTitle": guide[0].categoryTitle, "messageID": newMsgId, "category": guide[0].category}})
+			// })
             
-            message.channel.send("Category posted.")
+            // message.channel.send("Category posted.")
         })
     }
 }
