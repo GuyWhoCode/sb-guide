1a93e0e58c4fbaceaa8cca26a09490c39d0ea2a2
GuyWhoCodes <thatguywhocodes@gmail.com>
2020-12-29 21:03:28 -0800

    added a few more restricted commands

--- constants.js
+++ constants.js
@@ -13,7 +13,7 @@ const categorySchema = {
 const sbAlias = ["<#772942075301068820>", "sb", "skyblock", 'Skyblock', 'SB', 'SkyBlock']
 const dAlias = ["<#772944394542121031>","d", "dungeons", "dung", "Dungeons", "D", "dungeon", "Dungeon", "Dung"]
 const uAlias = ["u", "update", "UPDATE", "Update", "U"]
-const restrictedCmds = ['addcategory', 'addsection', 'approve', 'delete', 'edit', 'post', 'start']
+const restrictedCmds = ['addcategory', 'addsection', 'approve', 'delete', 'edit', 'post', 'start', 'pnda', 'ad']
 const verifiedRoles = ['Verified','VIP', 'VIP+', 'MVP', 'MVP+', 'MVP++']
 const cooldownCmds = ['sbsuggest', 'dsuggest', 'update', 'start']
 
