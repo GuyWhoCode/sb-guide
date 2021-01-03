16ecfc20c6fc04afb330974d0bdbfd0d74040b63
GuyWhoCodes <thatguywhocodes@gmail.com>
2021-01-03 07:45:50 -0800

    testing global variable

--- mongodb.js
+++ mongodb.js
@@ -1,13 +1,10 @@
 const mongoClient = require('mongodb').MongoClient
 const uri = "mongodb+srv://dbADMIN:"+ process.env.password + "@guide-info.e5dr4.mongodb.net/skyblockGuide?retryWrites=true&w=majority";
 var database;
-// const dbClient = new mongoClient(uri, { )
-mongoClient.connect(uri, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    poolSize: 10
-}, (err, db) => {
-    database = db
+const dbClient = new mongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true})
+
+dbClient.connect((err, data) => {
+    database = data.db("skyblockGuide")
 })
 module.exports = {
     database: database
