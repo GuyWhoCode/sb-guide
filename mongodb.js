7f25232dc4f2396b2186f95cebfc04b3b6962250
GuyWhoCodes <thatguywhocodes@gmail.com>
2021-01-03 07:33:31 -0800

    test initialization of database

--- mongodb.js
+++ mongodb.js
@@ -1,12 +1,11 @@
 const mongoClient = require('mongodb').MongoClient
 const uri = "mongodb+srv://dbADMIN:"+ process.env.password + "@guide-info.e5dr4.mongodb.net/skyblockGuide?retryWrites=true&w=majority";
-const dbClient = new mongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true})
-
 var database;
-dbClient.connect(async (err, db) => {
+const dbClient = new mongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
     database = db
 })
 
+console.log(database.collection("Guides").find({"category": "Skyblock"}))
 module.exports = {
     database: database
 }
