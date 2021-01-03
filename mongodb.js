8bc1e087cc4b6d9b080a339094545e0cd9e6b384
GuyWhoCodes <thatguywhocodes@gmail.com>
2021-01-03 07:37:13 -0800

    testing another way to write mongodb driver

--- mongodb.js
+++ mongodb.js
@@ -1,11 +1,14 @@
 const mongoClient = require('mongodb').MongoClient
 const uri = "mongodb+srv://dbADMIN:"+ process.env.password + "@guide-info.e5dr4.mongodb.net/skyblockGuide?retryWrites=true&w=majority";
 var database;
-const dbClient = new mongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
+// const dbClient = new mongoClient(uri, { )
+mongoClient.connect(uri, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    poolSize: 10
+}, (err, db) => {
     database = db
 })
-
-console.log(database.collection("Guides").find({"category": "Skyblock"}))
 module.exports = {
     database: database
 }
