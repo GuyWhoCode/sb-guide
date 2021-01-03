6a151b2261e29e0b61894f3a6649a8d2ec81195c
GuyWhoCodes <thatguywhocodes@gmail.com>
2021-01-03 07:59:47 -0800

    testing setting db?

--- mongodb.js
+++ mongodb.js
@@ -1,12 +1,14 @@
 const mongoClient = require('mongodb').MongoClient
 const uri = "mongodb+srv://dbADMIN:"+ process.env.password + "@guide-info.e5dr4.mongodb.net/skyblockGuide?retryWrites=true&w=majority";
 var database;
-const dbClient = new mongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true})
-
-dbClient.connect((err, data) => {
-    // console.log(data.collection("Guides").find({"category": "Skyblock"}))
-    console.log(data)
+// const dbClient = new mongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoClient.connect(uri, function(err, client) {
+    database = client.db("skyblockGuide")
 })
+// dbClient.connect((err, data) => {
+//     // console.log(data.collection("Guides").find({"category": "Skyblock"}))
+//     console.log(data)
+// })
 module.exports = {
     database: database
 }
