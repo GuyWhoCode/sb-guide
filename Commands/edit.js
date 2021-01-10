ec45d050f7e6642bb4dd7bd49b057ec9e3dd1322
GuyWhoCodes <thatguywhocodes@gmail.com>
2021-01-09 21:28:18 -0800

    debugging message

--- Commands/edit.js
+++ Commands/edit.js
@@ -34,36 +34,34 @@ module.exports = {
 			if (foundSection == false) return message.channel.send("The section that was given was incorrect. Remember to separate Section titles with more than 2 words with hyphens.")
 			//returns an error if the provided Section Name did not match anything in the Guide message
 			
-			// message.channel.send("Post the edited version below. This message will expire in 20 seconds. If you want to quit/cancel, type in `no` or `cancel`.\nHere is the original message as a reference: " + "```" + oldMessage + "```")
+			message.channel.send("Post the edited version below. This message will expire in 20 seconds. If you want to quit/cancel, type in `no` or `cancel`.\nHere is the original message as a reference: " + "```" + oldMessage + "```")
 			
-			// const filter = msg => msg.author.id === message.author.id && msg.content.length != 0
-			// const collector = message.channel.createMessageCollector(filter, {time: 20000})
+			const filter = msg => msg.author.id === message.author.id && msg.content.length != 0
+			const collector = message.channel.createMessageCollector(filter, {time: 20000})
 			var received = true
 			var newMsg = ""
 			//awaits new message for edit
-			// collector.on('collect', msg => {
-				// if (received && globalFunction.checkAliases(yesAlias, msg.content.trim())) {
-					//second collector for Confirmation. Edits the message.
-					// collector.stop()
+			collector.on('collect', msg => {
+				if (received && globalFunction.checkAliases(yesAlias, msg.content.trim())) {
+					// second collector for Confirmation. Edits the message.
+					collector.stop()
 
-					// embedMessage.fields[oldMsgID].value = newMsg + "\n\u200b"
+					embedMessage.fields[oldMsgID].value = newMsg + "\n\u200b"
 					var guideChannel = ""
 					categoryMsg[0].category === "Skyblock" ? guideChannel = message.guild.channels.cache.find(ch => ch.name === "skyblock-guide") : guideChannel = message.guild.channels.cache.find(ch => ch.name === "dungeons-guide-n-tips")
 			
 					embedMessage.timestamp = new Date()
 		    		guideChannel.messages.fetch({around: categoryMsg[0].messageID, limit: 1}).then(m => {
-						console.log(m.first())
-						// m.first().edit({embed: embedMessage})
+						m.first().edit({embed: embedMessage}).then(me => {message.channel.send("ID: " + me.id)})
 					})
 					
-					// let logChannel = message.guild.channels.cache.find(ch => ch.name === "guide-log")
-					// logChannel.send({embed: globalFunction.logAction(message.author.username, message.author.id, 'Edit', newMsg, categoryMsg[0].categoryTitle)})
+					let logChannel = message.guild.channels.cache.find(ch => ch.name === "guide-log")
+					logChannel.send({embed: globalFunction.logAction(message.author.username, message.author.id, 'Edit', newMsg, categoryMsg[0].categoryTitle)})
 
-					// guidesDB.updateOne({"categoryTitle": { $regex: new RegExp(categoryTitle, "i") }}, {$set: {"embedMessage": embedMessage, "categoryTitle": categoryMsg[0].categoryTitle, "messageID": categoryMsg[0].messageID, "category": categoryMsg[0].category}})
-					// message.channel.send("Message edited.")
+					guidesDB.updateOne({"categoryTitle": { $regex: new RegExp(categoryTitle, "i") }}, {$set: {"embedMessage": embedMessage, "categoryTitle": categoryMsg[0].categoryTitle, "messageID": categoryMsg[0].messageID, "category": categoryMsg[0].category}})
+					message.channel.send("Message edited.")
 
-				// } else 
-				if (globalFunction.checkAliases(noAlias, msg.content.trim()) || globalFunction.checkAliases(cancelAlias, msg.content.trim())) {
+				} else if (globalFunction.checkAliases(noAlias, msg.content.trim()) || globalFunction.checkAliases(cancelAlias, msg.content.trim())) {
 					//stops Edit process if given no/cancel alias
 					collector.stop()
 					message.channel.send("Process canceled.")
@@ -82,11 +80,11 @@ module.exports = {
 					} else message.channel.send("Please confirm the new message with `yes`. If you want to quit/cancel, type in `no` or `cancel`. " + "\n`" + newMsg + "`")
 				}
 					
-			// })
+			})
 
-			// collector.on('end', msg => {
-			// 	if (received == false) message.channel.send("The message has expired with no output.")
-			// })
+			collector.on('end', msg => {
+				if (received == false) message.channel.send("The message has expired with no output.")
+			})
 		})
 
 	},
