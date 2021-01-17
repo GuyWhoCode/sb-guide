0f342e51e10a61bd54784fd0c45c6046a3dd725e
GuyWhoCodes <thatguywhocodes@gmail.com>
2021-01-16 16:17:14 -0800

    missed one function

--- Commands/search.js
+++ Commands/search.js
@@ -3,14 +3,14 @@ const globalFunctions = require("../globalfunctions.js")
 
 module.exports = {
     name: "search",
-    execute(message, args) {
-        if (args.length == 0 || args[0] == undefined) return message.channel.send("See `g!search <Category-Name>`")
+    async execute(message, args) {
+        if (args.length == 0 || args[0] == undefined) return message.channel.send("See `g!search <Category Name>`")
         //checks if there is any bad input
         var categoryName = new RegExp(globalFunctions.translateCategoryName(args[0]), "i") 
         let guidesDB = dbClient.db("skyblockGuide").collection("Guides")
-        let guide = guidesDB.find( { "categoryTitle": { $regex: categoryName } }).toArray()
+        let guide = await guidesDB.find( { "categoryTitle": { $regex: categoryName } }).toArray()
         
-        if (guide[0] == undefined || guide.length > 1) return message.channel.send("The Category Title that was given was incorrect. Remember to separate Category titles with more than 2 words with hyphens.")
+        if (guide[0] == undefined || guide.length > 1) return message.channel.send("The Category Title that was given was incorrect.")
         //returns an error if the Category Title did not match anything in the database
         
         let guideMessage = guide[0].embedMessage
