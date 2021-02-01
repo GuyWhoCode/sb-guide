f7957633cba18edcf1e6b15291cc8d38705ae5ed
GuyWhoCodes <thatguywhocodes@gmail.com>
2021-02-01 08:28:10 -0800

    forgot async await

--- Commands/vote.js
+++ Commands/vote.js
@@ -1,13 +1,13 @@
 const {dbClient} = require("../mongodb.js")
 module.exports = {
     name: "vote",
-    execute(message, args) {
+    async execute(message, args) {
         if (args.length == 0 || args[0] == undefined || args[1] == undefined) return message.channel.send("`g!vote <Candidate Number> <Secret Code>`")
         message.delete()
         if (args[1].trim() != "mayor") return message.channel.send("You have the wrong secret word!")
         if (parseInt(args[0].trim()) < 0 || parseInt(args[0].trim()) > 3) return message.channel.send("That Candidate doesn't exist!")
         let voteDB = dbClient.db("skyblockGuide").collection("voting")
-        let findUser = voteDB.find({"user": message.author.id}).toArray()
+        let findUser = await voteDB.find({"user": message.author.id}).toArray()
         let user = findUser[0]
         if (user != undefined) return message.channel.send("You have already voted!")
 
