878591c3e610e8923764e34c40d23d0d90bbb363
GuyWhoCodes <thatguywhocodes@gmail.com>
2021-03-25 21:20:33 -0700

    forgot to reference

--- Commands/pnda.js
+++ Commands/pnda.js
@@ -1,9 +1,10 @@
 const aliases = ["panda", "henry"]
+const globalFunctions = require("../globalfunctions.js")
 module.exports = {
     name: "pnda",
     alises: aliases,
     execute(message, args) {
-        message.channel.send({embed: commandHelpEmbed("Pnda", aliases, Date.now(), "g!pnda", "Returns a ping for stoopid pnda")}) 
+        message.channel.send({embed: globalFunctions.commandHelpEmbed("Pnda", aliases, Date.now(), "g!pnda", "Returns a ping for stoopid pnda")}) 
         // message.channel.send("<@317751950441447435> stoopid pnda")
     }
 }
