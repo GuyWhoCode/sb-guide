8bdd283cfe05324d07c41bdfc7e8b438668d10d4
GuyWhoCode <thatguywhocodes@gmail.com>
2021-04-06 21:26:41 -0700

    testing fuse search results

--- Commands/search.js
+++ Commands/search.js
@@ -1,6 +1,14 @@
 const {dbClient} = require("../mongodb.js")
 const globalFunctions = require("../globalfunctions.js")
 const distance = require('jaro-winkler')
+const Fuse = require('fuse.js')
+const options = {
+    includeScore: true,
+    shouldSort: true,
+    keys: ["categoryTitle", "embedMessage.fields.name"]
+}
+let threshold = 0.60
+
 module.exports = {
     name: "search",
     async execute(message, args) {
@@ -9,8 +17,8 @@ module.exports = {
         //checks if there is any bad input
         let guidesDB = dbClient.db("skyblockGuide").collection("Guides")
         let guide = await guidesDB.find({}).toArray()
-
-        let threshold = 0.60
+        let fuseSearch = new Fuse(guide, options)
+        
         const parseQuery = query => {
             possibleQueries = {}
             guide.map(val => {
@@ -18,21 +26,23 @@ module.exports = {
                     .split(" ")
                     .map(word => distance(query, word, {caseSensitive: false}))
                     .reduce((prev, current) => prev + current)/(val.categoryTitle.split(" ").length)
+                
                 if (closeness > threshold) {
                     possibleQueries[val.categoryTitle] = closeness
                     possibleQueries[val.categoryTitle + " embed"] = val.embedMessage
                 }
             })
-        
-            var bestQuery = ""
+            
+            console.log(fuse.search(query))
+            var bestResult = ""
             Object.keys(possibleQueries)
                 .filter(val => !val.includes("embed"))
                 .map(val => {
-                    if (bestQuery == "" || possibleQueries[bestQuery] < possibleQueries[val]) {
-                        bestQuery = val 
+                    if (bestResult == "" || possibleQueries[bestResult] < possibleQueries[val]) {
+                        bestResult = val 
                     } 
                 })
-            return possibleQueries[bestQuery + " embed"]
+            return possibleQueries[bestResult + " embed"]
         }
         let guideMessage = parseQuery(searchQuery)
         // if (guide[0] == undefined || guide.length > 1) return message.channel.send("The Category Title that was given was incorrect.")
@@ -47,8 +57,7 @@ module.exports = {
 }
 
 // https://fusejs.io/
-// https://www.npmjs.com/package/jaro-winkler
-// const fuse = require('fuse.js')
+
 //fuse is backup algo.
 
 // Command to run test file for this is: node ./Commands/search.js
