8d814b0f3664206047f0463500d456f7a3a8e410
GuyWhoCode <thatguywhocodes@gmail.com>
2021-05-10 21:14:55 -0700

    testing another output

--- Commands/start.js
+++ Commands/start.js
@@ -1,10 +1,11 @@
 module.exports = {
 	name: 'start',
 	execute(message, args) {
-		var guideChannel = message.guild.channels.cache.find(ch => ch.name === "general")
-		guideChannel.messages.fetch({around: 841527319860740136, limit: 1}).then(m => {
+		var guideChannel = message.guild.channels.cache.find(ch => ch.name === "skyblock-guide")
+		guideChannel.messages.fetch({around: 799806929110302730, limit: 1}).then(m => {
 			console.log(m)
 		})
 		message.channel.send('Bot has started!')
 	},
 }
+
