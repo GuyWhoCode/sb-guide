2f8cfad2a68bc6202ccd1047f5773fc878efc002
GuyWhoCode <thatguywhocodes@gmail.com>
2021-05-10 21:19:05 -0700

    testing outdated message

--- Commands/start.js
+++ Commands/start.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'start',
 	execute(message, args) {
 		var guideChannel = message.guild.channels.cache.find(ch => ch.name === "skyblock-guide")
-		guideChannel.messages.fetch({around: 799806929110302730, limit: 1}).then(m => {
+		guideChannel.messages.fetch({around: 833719481989726290, limit: 1}).then(m => {
 			console.log(m)
 		})
 		message.channel.send('Bot has started!')
