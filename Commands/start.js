4a6bbe4347981f47847d6eebd26e48021492720c
GuyWhoCode <thatguywhocodes@gmail.com>
2021-05-14 15:25:39 -0700

    syntax error

--- Commands/start.js
+++ Commands/start.js
@@ -5,7 +5,7 @@ module.exports = {
 		// message.channel.send('Bot has started!')
 		globalFunctions.tableOfContents("Skyblock", message.guild.id)
             .then(val => message.guild.channels.cache.find(ch => ch.id === "807321396829093909")
-                    .send({embed: val})
+                    .send({embed: val}))
                     // .then(msg => findServer[0].sbTable = msg.id))
 	},
 }
