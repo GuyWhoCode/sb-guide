b597224ebe2c37d08f875428fef731832dad3fb9
GuyWhoCode <thatguywhocodes@gmail.com>
2021-05-14 15:25:01 -0700

    testing individual table of contents

--- Commands/start.js
+++ Commands/start.js
@@ -1,7 +1,12 @@
+const globalFunctions = require("../globalfunctions.js")
 module.exports = {
 	name: 'start',
 	execute(message, args) {
-		message.channel.send('Bot has started!')
+		// message.channel.send('Bot has started!')
+		globalFunctions.tableOfContents("Skyblock", message.guild.id)
+            .then(val => message.guild.channels.cache.find(ch => ch.id === "807321396829093909")
+                    .send({embed: val})
+                    // .then(msg => findServer[0].sbTable = msg.id))
 	},
 }
 
