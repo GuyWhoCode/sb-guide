aa2b8c33b31c91e89ed9206f82c2f2cdfa87b271
GuyWhoCode <thatguywhocodes@gmail.com>
2021-05-15 16:27:45 -0700

    forgot to include function declaration

--- Commands/start.js
+++ Commands/start.js
@@ -1,7 +1,8 @@
+const post = require("../post.js")
 module.exports = {
 	name: 'start',
 	execute(message, args) {
 		message.channel.send('Bot has started!')
-		
+		post.post(message, message.guild.id, "edit", "Skills Guide")
 	},
 }
